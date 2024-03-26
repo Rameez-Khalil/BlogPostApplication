6027b487243b4d7022808ed5f0847ac7f17d9099
rameez <rameezkhalil23@gmail.com>
2024-03-25 17:25:59 -0700

    admin tags, read data, push the data

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,56 @@
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.VIewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+
+    {
+        private BloggieDbContext bloggieDbContext; 
+
+        //CONSTRUCTOR INJECTION: 
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+        [HttpGet]
+
+        //AdminTags/Add
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+       
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+
+            //Mapping the tag.
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            //Store the tag.
+            bloggieDbContext.Tags.Add(tag);
+
+            //
+            bloggieDbContext.SaveChanges();
+            return RedirectToAction("List"); 
+        }
+
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List()
+        {
+            var tags = bloggieDbContext.Tags.ToList();
+
+            //The result list will be passed to our view.
+            return View(tags); 
+        }
+    }
+}
---
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Bloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    //Since EF core knows about the DbContext
+    public class BloggieDbContext : DbContext
+    {
+        //We will overrie this options parameters from program file.
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Notate the props that you want in database.
+        public DbSet<BlogPost> BlogPosts{ get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
